0311547acf231e315a15c16a7b4bf856db7ee238
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-05-30 20:03:15 +0100

    Meme generator
    Made the layout of the meme generator

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,10 @@
-const btn = document.getElementById('btn-1');
-function addParagraph(){
-    const paragraph = document.createElement('p');
-    paragraph.textContent = 'A new element has been created';
-    document.body.appendChild(paragraph);
-}
-//using an eventlistener to call the function
-btn.onclick = addParagraph;
+// const btn = document.getElementById('btn-1');
+// function addParagraph(){
+//     const paragraph = document.createElement('p');
+//     paragraph.textContent = 'A new element has been created';
+//     document.body.appendChild(paragraph);
+// }
+// //using an eventlistener to call the function
+// btn.onclick = addParagraph;
+
+
